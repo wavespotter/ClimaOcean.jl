c48e1d581ec048c732f849eddf1551149c8a63e2
Miguel Solano <miguel.solano.cordoba@gmail.com>
2025-04-09 15:46:26 -0700

    Bugfix

--- src/DataWrangling/MO/MO.jl
+++ src/DataWrangling/MO/MO.jl
@@ -141,7 +141,7 @@ const_MO = [
     5727.917, 
 ]
 
-MO_z = -MO_const_MO
+MO_z = -const_MO
 
 empty_MO_field(variable_name::Symbol; kw...) = empty_MO_field(Metadatum(variable_name, dataset=MO4Monthly()); kw...)
 
